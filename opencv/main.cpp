0e9c804b1faea7d076289e798b6ec9a31183a57b
benachourghailen@gmail.com <benachourghailen@gmail.com>
2021-09-23 14:33:24 +0100

    Update Line segmentation

--- opencv/main.cpp
+++ opencv/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <memory>
 #include <stdio.h>
@@ -14,20 +15,13 @@
 #define DEFAULT_WIDTH 1280
 #define IMG_SIZE 512
 
-using namespace std;
-using namespace cv;
-
+// PROTOTYPES
+cv::Mat frame_prediction(cv::Mat frame, torch::jit::Module model);
+torch::jit::Module load_model(std::string model_name);
 
 int main() {
   // Set torch module
-  cout << "OpenCV version : " << CV_VERSION << endl;
-
-  cout << "Major version : " << CV_MAJOR_VERSION << endl;
-
-  cout << "Minor version : " << CV_MINOR_VERSION << endl;
-
-  cout << "Subminor version : " << CV_SUBMINOR_VERSION << endl;
-
+  torch::jit::script::Module module;
   // OPENCV
   cv::VideoCapture cap;
   cv::Mat frame;
@@ -36,25 +30,68 @@ int main() {
   if (!cap.isOpened()) {
     std::cerr << "\nCannot open video\n";
   }
-  while(1){
-    Mat frame;
-    // Capture frame-by-frame
-    cap >> frame;
-    // If the frame is empty, break immediately
-    if (frame.empty())
-      break;
-    // Display the resulting frame
-    imshow( "Frame", frame );
-    // Press  ESC on keyboard to exit
-    char c=(char)waitKey(25);
-    if(c==27)
-      break;
-  }
-  // When everything done, release the video capture object
-  cap.release();
-  // Closes all the frames
-  destroyAllWindows();   
-  return 0;
 
+  std::cout << "\nPress spacebar to terminate\n";
+  // Load Model
+  try {
+    module = load_model("quantized_lanesNet.pt");
+  } catch (const c10::Error &e) {
+    std::cerr << "error loading the model\n";
+  }
+
+  for (;;) {
+    cap.read(frame);
+    if (frame.empty()) {
+      std::cerr << "\nError:Blank Frame\n";
+    }
+
+    frame = frame_prediction(frame, module);
+    cv::imshow("video", frame);
+
+    if (cv::waitKey(1) >= 27) {
+      break;
+    }
+  }
 }
 
+torch::jit::Module load_model(std::string model_name) {
+  std::string directory = "../models/" + model_name;
+  torch::jit::Module module = torch::jit::load(directory);
+ // module.to(torch::kCUDA);
+  module.eval();
+  std::cout << "MODEL LOADED";
+  return module;
+}
+
+cv::Mat frame_prediction(cv::Mat frame, torch::jit::Module model) {
+  // Needed for Overlay
+  double alpha = 0.1;
+  double beta = (1 - alpha);
+  cv::Mat frame_copy, dst;
+  // Torch model input
+  std::vector<torch::jit::IValue> input;
+  // Mean and std
+  std::vector<double> mean = {0.406, 0.456, 0.485};
+  std::vector<double> std = {0.225, 0.224, 0.229};
+  cv::resize(frame, frame, cv::Size(IMG_SIZE, IMG_SIZE));
+  frame_copy = frame;
+  frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
+  // CV2 to Tensor
+  torch::Tensor frame_tensor =
+      torch::from_blob(frame.data, {1, IMG_SIZE, IMG_SIZE, 3});
+  frame_tensor = frame_tensor.permute({0, 3, 1, 2});
+  frame_tensor = torch::data::transforms::Normalize<>(mean, std)(frame_tensor);
+  //frame_tensor = frame_tensor.to(torch::kCUDA);
+  input.push_back(frame_tensor);
+  // Forward Pass
+  auto pred = model.forward(input).toTensor().detach().to(torch::kCPU);
+  pred = pred.mul(100).clamp(0.255).to(torch::kU8);
+  // Tensor -> CV2
+  cv::Mat output_mat(cv::Size{IMG_SIZE, IMG_SIZE}, CV_8UC1, pred.data_ptr());
+  cv::cvtColor(output_mat, output_mat, cv::COLOR_GRAY2RGB);
+  cv::applyColorMap(output_mat, output_mat, cv::COLORMAP_TWILIGHT_SHIFTED);
+  // OVERLAY ORIGINAL FRAME AND PREDICTION
+  cv::addWeighted(frame_copy, alpha, output_mat, beta, 0.0, dst);
+  cv::resize(dst, dst, cv::Size(DEFAULT_WIDTH, DEFAULT_HEIGHT));
+  return dst;
+}
